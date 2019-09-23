3e43d4d6ac7ffebdc34e6c0ae90ddf1529c37083
Dewald de Jager <dewald.dejager@entelect.co.za>
2019-09-23 11:43:25 +0200

    Add scanning, debug output, check for existing table and do minor cleanup

--- src/main/java/com/dearvolt/awsplayground/dynamodb/DynamoPlayground.java
+++ src/main/java/com/dearvolt/awsplayground/dynamodb/DynamoPlayground.java
@@ -1,3 +1,5 @@
+package com.dearvolt.awsplayground.dynamodb;
+
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -35,7 +37,7 @@ enum AttributeNames {
 }
 
 public class DynamoPlayground {
-    private String termsAndConditionsTableName = "TermsAndConditionsSubmissions";
+    private final String termsAndConditionsTableName = "TermsAndConditionsSubmissions";
     private DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
     private AmazonDynamoDB dbClient = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
             new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "eu-west-1")
@@ -44,8 +46,14 @@ public class DynamoPlayground {
     public static void main(String[] args) {
         System.out.println("xD");
         DynamoPlayground playground = new DynamoPlayground();
-//        playground.createTestTable();
+        try {
+            playground.createTestTable();
+        } catch (ResourceInUseException e) {
+            System.err.println("Table already exists. Moving on...\n");
+        }
+        playground.createTestItems();
         playground.describeTable();
+        playground.getAllItems();
     }
 
     private boolean hasUserAcceptedTermsAndConditions(String digitalId, int version) {
@@ -62,6 +70,7 @@ public class DynamoPlayground {
     }
 
     private void createTestTable() {
+        System.out.println("\nCREATING TABLE\n");
         CreateTableRequest request = new CreateTableRequest()
                 .withAttributeDefinitions(
                         AttributeNames.DIGITAL_ID.toAttributeDefinition(),
@@ -72,6 +81,7 @@ public class DynamoPlayground {
                 .withBillingMode("PAY_PER_REQUEST")
                 .withTableName(termsAndConditionsTableName);
         dbClient.createTable(request);
+        System.out.println("\nDONE CREATING TABLE\n");
     }
 
     private void createTestItems() {
@@ -82,11 +92,15 @@ public class DynamoPlayground {
     private void describeTable() {
         System.out.println("\nDESCRIBING TABLE\n");
         System.out.println(dbClient.describeTable(termsAndConditionsTableName));
-        System.out.println();
+        System.out.println("\nDONE DESCRIBING TABLE\n");
     }
 
     // aws dynamodb scan --table-name TermsAndConditionsSubmissions --endpoint-url http://localhost:8000
     private void getAllItems() {
-        // TODO
+        System.out.println("\nSCANNING TABLE\n");
+        ScanRequest request = new ScanRequest()
+                .withTableName(termsAndConditionsTableName);
+        System.out.println(dbClient.scan(request));
+        System.out.println("\nDONE SCANNING TABLE\n");
     }
 }
